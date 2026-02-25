61f1f331704150a65c7c1671e0f08d4c5b1a0fcf
afsalmern <afsal@intersmart.in>
2026-02-25 12:51:16 +0530

    added intergration of bought together section in product detaill page

--- src/components/blocks/product/frequently-bought-card.jsx
+++ src/components/blocks/product/frequently-bought-card.jsx
@@ -3,15 +3,14 @@ import Image from "next/image";
 import { Heading } from "../../utils/heading";
 import { Text } from "../../utils/text";
 import Link from "next/link";
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import { motion } from "motion/react";
 import { Skeleton } from "../../ui/skeleton";
-import { Button } from "@/components/ui/button";
 import { Check } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-export default function FrequentlyBoughtCard({ product }) {
-  const [selected, setSelected] = useState(true);
+export default function FrequentlyBoughtCard({ product, locale, selected = true, onToggle }) {
+  const productUrl = product?.query_params ? `/${locale}/products/${product?.base_slug}${product?.query_params}` : "#";
 
   return (
     <Suspense fallback={<FrequentlyBoughtCardSkelton />}>
@@ -20,36 +19,24 @@ export default function FrequentlyBoughtCard({ product }) {
           <motion.button
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
-            onClick={() => setSelected(!selected)}
+            onClick={onToggle}
             className="w-3 xl:w-3.5 2xl:w-4.5 aspect-square bg-white flex items-center justify-center absolute z-2 top-1 xl:top-2 left-1 xl:left-2"
           >
-            <Check
-              className={cn("size-2 2xl:size-3", selected ? "text-black" : "text-white")}
-            />
+            <Check className={cn("size-2 2xl:size-3", selected ? "text-black" : "text-white")} />
           </motion.button>
-          {!product?.isStock && (
-            <div className="w-full h-full bg-[#f4f4f4]/90 flex items-center justify-center absolute z-2 inset-0">
-              <Button
-                variant={"black"}
-                disabled={true}
-                className="min-w-[100px] xl:min-w-[120px] 2xl:min-w-[200px] disabled:opacity-100 rounded-[2px] m-auto"
-              >
-                Out of Stock
-              </Button>
-            </div>
-          )}
+
           <Image
-            src={product?.media?.path}
-            alt={product?.media?.alt}
+            src={product?.variant_image}
+            alt={locale === "ar" ? product?.title_ar : product?.title}
             width={550}
             height={440}
             className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
             quality={90}
           />
-          {product?.hoverMedia && (
+          {product?.hover_image && (
             <Image
-              src={product?.hoverMedia?.path}
-              alt={product?.hoverMedia?.alt}
+              src={product?.hover_image}
+              alt={locale === "ar" ? product?.title_ar : product?.title}
               width={550}
               height={440}
               quality={100}
@@ -63,25 +50,25 @@ export default function FrequentlyBoughtCard({ product }) {
             size="none"
             className="text-[8px] xl:text-[8px] 2xl:text-[10px] 3xl:text-[12px] leading-normal font-light truncate text-[#bbbcbc] mb-0.5"
           >
-            <Link href={product?.slug}>{product?.category}</Link>
+            <Link href={product?.query_params ? `/${locale}/product?.query_params` : "#"}>
+              {" "}
+              {product?.categories?.map((cat) => (locale === "en" ? cat.name : cat.name_ar)).join(", ")}
+            </Link>
           </Heading>
           <Heading
             as="div"
             size="none"
             className="text-[10px] xl:text-[10px] 2xl:text-[12px] 3xl:text-[14px] leading-normal font-normal truncate text-[#282828] mb-0.5"
           >
-            <Link href={product?.slug}>{product?.name}</Link>
+            <Link href={productUrl}>{locale == "ar" ? product?.title_ar : product?.title}</Link>
           </Heading>
           <Text
             as="div"
             size="none"
             className="text-[11px] xl:text-[10px] 2xl:text-[12px] 3xl:text-[14px] leading-normal font-normal truncate text-[#282828]"
           >
-            <Link href={product?.slug}>
-              AED {product?.price}{" "}
-              <span className="text-[8px] 2xl:text-[10px] font-light text-[#bbbcbc] ">
-                Inc Tax
-              </span>
+            <Link href={productUrl}>
+              AED {product?.price} <span className="text-[8px] 2xl:text-[10px] font-light text-[#bbbcbc] ">Inc Tax</span>
             </Link>
           </Text>
         </div>
