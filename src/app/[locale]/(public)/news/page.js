b50289cbba5285d2bce7366ae31d5d1dab9b4447
Nikhil-Govind-Inter <nikhil@intersmart.in>
2026-04-13 14:48:38 +0530

    fixed pagination issues

--- src/app/[locale]/(public)/news/page.js
+++ src/app/[locale]/(public)/news/page.js
@@ -42,12 +42,14 @@ export default async function NewsPage({ params, searchParams }) {
   const slug = locale==="en" ? "News" : "أخبار";
 
   const { heroData } = cmsResult.data;
-  const blogListData = blogListResult.data || { blog: [], pagination: {} };
+  const blogListData = blogListResult.data || { news: [], pagination: {} };
 
   return (
     <>
       <NewsHero locale={locale} data={heroData} slug={slug} type={"news"} />
+      {blogListResult?.data?.news && 
       <NewsList locale={locale} data={blogListData} />
+      }
     </>
   );
 }
