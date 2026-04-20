364b7b517c5ef6b32bbc3b2c1c423fbbbe445b01
afsalmern <afsal@intersmart.in>
2026-04-20 18:14:58 +0530

    fix: update parameter name in HeaderClient component

--- src/components/clients/HeaderClient.js
+++ src/components/clients/HeaderClient.js
@@ -2,6 +2,6 @@
 import dynamic from "next/dynamic";
 const Header = dynamic(() => import("@/components/layout/header"), { ssr: true });
 
-export default function HeaderClient({ locale, navigationData, headerData }) {
-  return <Header locale={locale} navigationData={navigationData} data={headerData} />;
+export default function HeaderClient({ locale, navigationData, data }) {
+  return <Header locale={locale} navigationData={navigationData} data={data} />;
 }
