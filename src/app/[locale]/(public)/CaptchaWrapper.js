c310c5bc7c14a8b08f07bcda5c89b49f6c434060
afsalmern <afsal@intersmart.in>
2026-04-22 18:53:51 +0530

    added defer for captcha

--- src/app/[locale]/(public)/CaptchaWrapper.js
+++ src/app/[locale]/(public)/CaptchaWrapper.js
@@ -3,22 +3,26 @@
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export default function RecaptchaProvider({ children }) {
+  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
 
-    const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
-
-    if(!siteKey) {
-        console.error("RECAPTCHA_SITE_KEY is not defined");
-        return <>{children}</>;
-    }
+  if (!siteKey) {
+    console.error("RECAPTCHA_SITE_KEY is not defined");
+    return <>{children}</>;
+  }
 
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+      // scriptProps={{
+      //   async: false,
+      //   defer: false,
+      //   appendTo: "head",
+      //   nonce: undefined,
+      // }}
       scriptProps={{
-        async: false,
-        defer: false,
+        async: true,
+        defer: true,
         appendTo: "head",
-        nonce: undefined,
       }}
     >
       {children}
